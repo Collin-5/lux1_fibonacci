938230b5a9b2fcdbb52363dcc88a0e943ae1cab0
Collin-5 <collins380@gmail.com>
2022-02-13 20:38:00 +0300

    adding fibonacci function

---
+++ README.md
@@ -0,0 +1,1 @@
+thi function checks if a number is fibonacci
---
+++ fibonacci.py
@@ -0,0 +1,9 @@
+import math
+
+
+# check if number is fibonacci
+def isFibonacci(num):
+    i = int(math.sqrt(num))
+    return i*i == num
+
+print(isFibonacci(4))
